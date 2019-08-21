db21faffa87f140ef33e01a15eeb7dfb68f4db56
Tim Schaefer <ts+code@rcmd.org>
2019-08-21 14:43:27 +0200

    add initial version of code an package metadata files

---
+++ R/read_fs_curv_file.R
@@ -0,0 +1,48 @@
+#' @title Read file in FreeSurfer curv format
+#'
+#' @description Reads vertex-wise brain surface data from a file in FreeSurfer binary 'curv' format. For a subject (MRI image pre-processed with FreeSurfer) named 'bert', an example file would be 'bert/surf/lh.thickness', which contains n values. Each value represents the cortical thickness at the respective vertex in the brain surface mesh of bert.
+#'
+#' @param filepath, string. Full path to the input curv file.
+#'
+#' @return data, vector of floats. The brain morphometry data, one value per vertex.
+#'
+#' @examples
+#'     curv.file = file.path(path.expand("~"), "software, "freesurfer", "subjects", "bert", "surf", "lh.thickness")
+#'     cortical.thickness = read_fs_curv_file(curv.file);
+#'
+#' @export
+read_fs_curv_file <- function(filepath) {
+    MAGIC_FILE_TYPE_NUMBER = 16777215;
+    fh = file(filepath, "rb");
+
+    magic_byte = fread3(fh);
+    if (magic_byte != MAGIC_FILE_TYPE_NUMBER) {
+        stop(sprintf("Magic number mismatch. The given file '%s' is not a valid FreeSurfer 'curv' format file in new binary format. (Hint: This function is designed to read files like 'lh.area' in the 'surf' directory of a pre-processed FreeSurfer subject.)\n", filepath));
+    }
+    num_verts = readBin(fh, integer(), n = 1, endian = "big");
+    num_faces = readBin(fh, integer(), n = 1, endian = "big");
+    values_per_vertex = readBin(fh, integer(), n = 1, endian = "big");
+    data = readBin(fh, numeric(), size = 4, n = num_verts, endian = "big");
+    close(fh);
+    return(data);
+}
+
+
+#' @title Read 3-byte integer.
+#'
+#' @description Read a 3-byte integer from a binary file handle. Advances the pointer accordingly.
+#'
+#' @param filehandle: file handle
+#'
+#' @return integer: The read integer.
+#'
+#' @examples
+#'
+#' @keywords internal
+fread3 <- function(filehandle) {
+    b1 = readBin(filehandle, integer(), size=1, signed = FALSE);
+    b2 = readBin(filehandle, integer(), size=1, signed = FALSE);
+    b3 = readBin(filehandle, integer(), size=1, signed = FALSE);
+    res = bitwShiftL(b1, 16) + bitwShiftL(b2, 8) + b3;
+    return(res);
+}
---
+++ R/read_fs_mgh_file.R
@@ -0,0 +1,123 @@
+#' @title Read file in FreeSurfer MGH or MGZ format
+#'
+#' @description Reads multi-dimensional brain imaging data from a file in FreeSurfer binary MGH or MGZ format. The MGZ format is just a gzipped version of the MGH format. For a subject (MRI image pre-processed with FreeSurfer) named 'bert', an example file would be 'bert/mri/T1.mgz', which contains a 3D brain scan of bert.
+#'
+#' @param filepath, string. Full path to the input MGZ or MGH file.
+#'
+#' @return data, multi-dimensional array. The brain imaging data, one value per voxel. The data type and the dimensions depend on the data in the file, they are read from the header.
+#'
+#' @examples
+#'     brain_image = file.path(path.expand("~"), "software, "freesurfer", "subjects", "bert", "mri", "T1.mgz")
+#'     voxel_data = read_fs_mgh_file(brain_image);
+#'
+#' @export
+read_fs_mgh_file <- function(filepath, is_gzipped = "AUTO") {
+
+    if(typeof(is_gzipped) == "logical") {
+        is_gz = is_gzipped;
+    } else if (typeof(is_gzipped) == "character") {
+        if(is_gzipped == "AUTO") {
+            nc = nchar(filepath);
+            num_chars_to_inspect = 3; # last 3 chars
+            if(nc >= 3) {
+                ext = substr(filepath, nchar(filepath)-num_chars_to_inspect+1, nchar(filepath));
+                if(tolower(ext) == "mgz" || tolower(ext) == ".gz") {
+                    is_gz = TRUE;
+                } else {
+                    is_gz = FALSE;
+                }
+            } else {
+                warning(sprintf("Argument 'is_gzipped set' to 'AUTO' but file name is too short (%d chars) to determine compression from last %d characters, assuming gz-compressed file.\n", nc, num_chars_to_inspect));
+                is_gz = TRUE;
+            }
+        } else {
+            stop("Argument 'is_gzipped' must be 'AUTO' if it is a string.\n");
+        }
+    } else {
+        stop(sprintf("ERROR: Argument is_gzipped must be logical (TRUE or FALSE) or 'AUTO'.\n"));
+    }
+
+    if (is_gz) {
+         cat(sprintf("*Parsing header of file '%s', assuming it is gz-compressed.\n", filepath));
+        fh = gzfile(filepath, "rb");
+    }
+    else {
+        cat(sprintf("*Parsing header of file '%s', assuming it is NOT gz-compressed.\n", filepath));
+        fh = file(filepath, "rb");
+    }
+
+    v = readBin(fh, integer(), n = 1, endian = "big");
+    ndim1 = readBin(fh, integer(), n = 1, endian = "big");
+    ndim2 = readBin(fh, integer(), n = 1, endian = "big");
+    ndim3  = readBin(fh, integer(), n = 1, endian = "big");
+    nframes = readBin(fh, integer(), n = 1, endian = "big");
+    dtype = readBin(fh, integer(), n = 1, endian = "big");
+    dof = readBin(fh, integer(), n = 1, endian = "big");
+
+
+    cat(sprintf(" v=%d, ndim1=%d, ndim2=%d, ndim3=%d, nframes=%d, type=%d, dof=%d.\n", v, ndim1, ndim2, ndim3, nframes, dtype, dof));
+
+
+    unused_header_space_size_left = 256;
+
+    ras_good_flag = readBin(fh, numeric(), n = 1, endian = "big");
+    if(ras_good_flag == 1) {
+        cat(sprintf(" 'RAS good' flag is set, reading RAS information.\n"));
+        delta  = readBin(filehandle, float(), n = 3, endian = "big")
+        Mdc    = readBin(filehandle, float(), n = 9, endian = "big")
+        Pxyz_c = readBin(filehandle, float(), n = 3, endian = "big")
+        RAS_space_size = (3*4 + 4*3*4);
+        cat(sprintf(" Read %d bytes of RAS information.\n", RAS_space_size));
+        unused_header_space_size_left = unused_header_space_size_left - RAS_space_size;
+    } else {
+        cat(sprintf(" 'RAS good' flag is NOT set, no RAS information to read.\n"));
+    }
+    cat(sprintf(" There are %d unused header bytes left that will be skipped (data starts at fixed index).\n", unused_header_space_size_left));
+
+    # Skip to end of header/beginning of data
+    seek(fh, where = unused_header_space_size_left, origin = "current");
+
+    nv = ndim1 * ndim2 * ndim3 * nframes;   # number of voxels
+    volsz = c(ndim1, ndim2, ndim3, nframes);
+    cat(sprintf(" Expecting %d voxels total.\n", nv));
+
+    # Determine size of voxel data, depending on dtype from header above
+    MRI_UCHAR = 0;
+    MRI_INT = 1;
+    MRI_FLOAT = 3;
+    MRI_SHORT = 4;
+
+    dt_explanation = "0=MRI_UCHAR; 1=MRI_INT; 3=MRI_FLOAT; 3=MRI_SHORT";
+
+    # Determine number of bytes per voxel
+    if(dtype == MRI_FLOAT) {
+        nbytespervox = 4;
+        data = readBin(fh, numeric(), size = nbytespervox, n = nv, endian = "big");
+    } else if(dtype == MRI_UCHAR) {
+        nbytespervox = 1;
+        data = readBin(fh, integer(), size = nbytespervox, n = nv, signed = FALSE, endian = "big");
+    } else if (dtype == MRI_SHORT) {
+        nbytespervox = 2;
+        data = readBin(fh, integer(), size = nbytespervox, n = nv, endian = "big");
+    } else if (dtype == MRI_INT) {
+        nbytespervox = 4;
+        data = readBin(fh, int(), size = nbytespervox, n = nv, endian = "big");
+    } else {
+       stop(sprintf(" ERROR: Unexpected data type found in header. Expected one of {0, 1, 3, 4} (%s) but got %d.\n", dt_explanation, dtype));
+    }
+    cat(sprintf(" Data type found in header is %d (%s), with %d bytes per voxel.\n", dtype, dt_explanation, nbytespervox));
+
+    num_read = prod(length(data));
+    if (num_read == nv) {
+        cat(sprintf(" OK, read %d voxel values as expected.\n", num_read));
+    } else {
+        cat(sprintf(" ERROR: read %d voxel values but expected to read %d.\n", num_read, nv));
+        quit(status=1);
+    }
+
+    # Reshape to expected dimensions
+    data = array(data, dim = c(ndim1, ndim2, ndim3, nframes));
+    cat(sprintf(" Reshaped array to expected dimensions: %s\n", paste(dim(data), collapse = ' ')));
+    close(fh);
+    return(data);
+}
