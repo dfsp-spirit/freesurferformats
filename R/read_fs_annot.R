f3755899901d7f06a3b3ef0fa2fe985aec2caad1
Tim Schaefer <ts+code@rcmd.org>
2019-09-06 17:02:26 +0200

    add empty file, gotta go now. WIP.

---
+++ R/read_fs_annot.R
@@ -0,0 +1,14 @@
+#' @title Read file in FreeSurfer annotation format
+#'
+#' @description Read a data annotation file in FreeSurfer format. Such a file assigns a label and a color to each vertex of a brain surface. The assignment of labels to vertices is based on at atlas or brain parcellation file. Typically the atlas is available for some standard template subject, and the labels are assigned to another subject by registering it to the template.
+#'    For a subject (MRI image pre-processed with FreeSurfer) named 'bert', an example file would be 'bert/label/lh.aparc.annot', which contains the annotation based on the Desikan-Killiany Atlas for the left hemisphere of bert.
+#'
+#' @param filepath, string. Full path to the input curv file.
+#'
+#' @return ???
+#'
+#'
+#' @export
+read.fs.annot <- function(filepath) {
+    return(data);
+}
