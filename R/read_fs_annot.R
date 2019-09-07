84aa548b2c4b0bc522f54a626731e01a7916728e
Tim Schäfer <ts+code@rcmd.org>
2019-09-07 12:57:15 +0200

    work on function to read annotation files

--- R/read_fs_annot.R
+++ R/read_fs_annot.R
@@ -10,5 +10,41 @@
 #'
 #' @export
 read.fs.annot <- function(filepath) {
+
+    fh = file(filepath, "rb");
+
+    num_verts_and_labels = readBin(fh, integer(), n = 1, endian = "big");
+    verts_and_labels = readBin(fh, integer(), n = num_verts_and_labels, endian = "big");
+
+    verts = verts_and_labels[seq(1L, length(verts_and_labels), 2L)]
+    labels = verts_and_labels[seq(2L, length(verts_and_labels), 2L)]
+
+    has_colortable = readBin(fh, integer(), n = 1, endian = "big");
+
+    if (has_colortable) {
+        ctable_num_entries = readBin(fh, integer(), n = 1, endian = "big");
+
+        if(ctable_num_entries > 0) {
+          # num_colortable_entries is really the number of entries
+          colortable <- list("num_entries" = num_colortable_entries)
+          ctable_length = readBin(fh, integer(), n = 1, endian = "big");
+
+          ctab_orig = readBin(fh, char(), n = ctable_length, endian = "big");
+          ctab_orig = ctab_orig[1:length(ctab_orig)-1]  # remove last element
+
+        } else {
+          # num_colortable_entries is a version code (actually, the abs value is).
+          version = -num_colortable_entries;
+          if(version == 2) {
+
+          }
+          else {
+              error("Unsupported annotation file version.");
+          }
+
+        }
+
+    }
+
     return(data);
 }
