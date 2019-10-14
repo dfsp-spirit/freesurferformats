e188d003be416398d3e4a3c916bd63464b194f89
Tim Schaefer <ts+code@rcmd.org>
2019-10-14 10:32:21 +0200

    add minor doc part

--- R/read_fs_annot.R
+++ R/read_fs_annot.R
@@ -7,7 +7,7 @@
 #'
 #' @param empty_label_name, string. The region name to assign to regions with empty name. Defaults to 'unknown'. Set to NULL if you want to keep the empty region name.
 #'
-#' @return named list, enties are: "vertices" vector of n vertex indices, starting with 0. "label_codes": vector of n integers, each entry is a color code, i.e., a value from the 5th column in the table structure included in the "colortable" entry (see below). "label_names": the n brain structure names for the vertices, already retrieved from the colortable using the code.
+#' @return named list, enties are: "vertices" vector of n vertex indices, starting with 0. "label_codes": vector of n integers, each entry is a color code, i.e., a value from the 5th column in the table structure included in the "colortable" entry (see below). "label_names": the n brain structure names for the vertices, already retrieved from the colortable using the code. "hex_colors_rgb": Vector of hex color for each vertex.
 #'      The "colortable" is another named list with 3 entries: "num_entries": int, number of brain structures. "struct_names": vector of strings, the brain structure names. "table": numeric matrix with num_entries rows and 5 colums. The 5 columns are: 1 = color red channel, 2=color blue channel, 3=color green channel, 4=color alpha channel, 5=unique color code. "colortable_df": The same information as a dataframe. Contains the extra columns "hex_color_string_rgb" and "hex_color_string_rgba" that hold the color as an RGB(A) hex string, like "#rrggbbaa".
 #'
 #' @examples
@@ -80,7 +80,8 @@ read.fs.annot <- function(filepath, empty_label_name="unknown") {
                 return_list$hex_colors_rgb = hex_colors_rgb;
             }
             else {
-                stop(sprintf("Unsupported annotation file version '%d'.", version));
+                stop(sprintf("Unsupported annotation file version '%d'.\n", version));
+                # TODO: Turns out this is still in use, so implement this. An example file is fsaverage/label/lh.aparc.a2005s.annot.
             }
         }
 
@@ -155,4 +156,3 @@ readcolortable <- function(fh, ctable_num_entries) {
 
     return(colortable);
 }
-
