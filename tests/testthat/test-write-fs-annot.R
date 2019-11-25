f8477c5d1d3c083301c59f713ae6ddb917252b18
Tim Schaefer <ts+code@rcmd.org>
2019-11-25 11:16:16 +0100

    extend unit tests

--- tests/testthat/test-write-fs-annot.R
+++ tests/testthat/test-write-fs-annot.R
@@ -10,6 +10,13 @@ test_that("An colortable from an annotation can be written to a text file in Fre
   written_cmap_df = read.fs.colortable(output_file);
   expect_equal(nrow(written_cmap_df), 36);
   expect_equal(ncol(written_cmap_df), 6);
+
+  region_unknown = subset(written_cmap_df, written_cmap_df$struct_name == "unknown");
+  expect_equal(region_unknown$struct_index, 0);
+  expect_equal(region_unknown$r, 25);
+  expect_equal(region_unknown$g, 5);
+  expect_equal(region_unknown$b, 25);
+  expect_equal(region_unknown$a, 0);
 })
 
 
