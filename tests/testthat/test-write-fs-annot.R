f0fa74d572eaa7b4c83ac0b59812e981b81572d6
Tim Schaefer <ts+code@rcmd.org>
2019-11-25 13:58:07 +0100

    add unit test for writing colortable LUT files

--- tests/testthat/test-write-fs-annot.R
+++ tests/testthat/test-write-fs-annot.R
@@ -1,22 +1,37 @@
 
 
-test_that("An colortable from an annotation can be written to a text file in FreeSurfer LUT format.", {
+test_that("A colortable from an annotation can be written to a text file in FreeSurfer LUT format.", {
   annotfile = system.file("extdata", "lh.aparc.annot.gz", package = "freesurferformats", mustWork = TRUE);
   annot = read.fs.annot(annotfile);
 
-  output_file = tempfile();
+  output_file = tempfile(fileext = ".txt");
   write.fs.colortable.from.annot(output_file, annot);
 
-  written_cmap_df = read.fs.colortable(output_file);
-  expect_equal(nrow(written_cmap_df), 36);
-  expect_equal(ncol(written_cmap_df), 6);
+  written_colortable = read.fs.colortable(output_file);
+  expect_equal(nrow(written_colortable), 36);
+  expect_equal(ncol(written_colortable), 6);
 
-  region_unknown = subset(written_cmap_df, written_cmap_df$struct_name == "unknown");
+  region_unknown = subset(written_colortable, written_colortable$struct_name == "unknown");
   expect_equal(region_unknown$struct_index, 0);
   expect_equal(region_unknown$r, 25);
   expect_equal(region_unknown$g, 5);
   expect_equal(region_unknown$b, 25);
   expect_equal(region_unknown$a, 0);
+})
+
+
+test_that("A colortable can be written to a text file in FreeSurfer LUT format.", {
+
+  colortable_df = data.frame("struct_index"=c(0, 1), "struct_name"=c("struct1", "struct2"), "r"=c(80, 100), "g"=c(50, 40), "b"=c(250, 200), "a"=c(0, 0), stringsAsFactors = FALSE);
+
+  output_file = tempfile(fileext = ".txt");
+  write.fs.colortable(output_file, colortable_df);
+
+  written_colortable = read.fs.colortable(output_file);
+
+  expect_equal(ncol(colortable_df), ncol(written_colortable));
+  expect_equal(nrow(colortable_df), nrow(written_colortable));
+  expect_equal(colortable_df, written_colortable);
 })
 
 
@@ -30,4 +45,5 @@ test_that("An annotation can be written in binary v2 format and read again.", {
   annot2 = read.fs.annot(output_file);
 
   expect_equal(annot$vertices, annot2$vertices);
+  expect_equal(annot, annot2);
 })
