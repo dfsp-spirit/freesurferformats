c38cb53b9a3fe7ce2ae430846ccdc9a1c431473e
Tim Schaefer <ts+code@rcmd.org>
2020-01-16 15:49:47 +0100

    implement format-agnostic surface file reading function, as discussed in #9

--- R/read_fs_surface.R
+++ R/read_fs_surface.R
@@ -1,8 +1,163 @@
+#' @title S3 method to read a surface file, tries all surface formats.
+#'
+#' @description With the default settings, one can read files in the following surface formats: 1) FreeSurfer binary surface format (e.g., `surf/lh.white`). 2) FreeSurfer ASCII surface format (e.g., `surf/lh.white,asc`). 3) GIFTI surface format, only if package `gifti` is installed. See \code{\link[gifti]{read_gifti}} for details. Feel free to implement additional methods. Hint:keep in mind that they should return one-based indices.
+#'
+#' @param filepath character string, the full path to the input surface file.
+#'
+#' @param methods list of character strings, the formats to try. Each of these must have a function called \code{read_nisurface.<method>}, which must return an `fs.surface` instance on success.
+#'
+#' @param ... parameters passed on to the individual methods
+#'
+#' @return an instance of `fs.surface`, read from the file. See \code{\link[freesurferformats]{read.fs.surface}} for details. If none of the reader methods succeed, an error is raised.
+#'
+#' @family mesh functions
+#'
+#' @examples
+#'     surface_file = system.file("extdata", "lh.tinysurface",
+#'                             package = "freesurferformats", mustWork = TRUE);
+#'     mesh = read_nisurface(surface_file);
+#'     mesh;
+#'
+#' @export
+read_nisurface <- function(filepath, methods=c('fsnative', 'fsascii', 'gifti'), ...) {
+  if(!file.exists(filepath)) {
+    stop(sprintf("Cannot read neuroimaging surface, file '%s' does not exist.\n", filepath));
+  }
+
+  class(filepath) <- c(methods, class(filepath));
+  UseMethod('read_nisurface', object = filepath);
+}
+
+
+#' @title Read a FreeSurfer ASCII surface file.
+#'
+#' @param filepath character string, the full path to the input surface file.
+#'
+#' @param ... parameters passed to \code{\link[freesurferformats]{read.fs.surface}}.
+#'
+#' @return an instance of `fs.surface`, read from the file. See \code{\link[freesurferformats]{read.fs.surface}} for details. If none of the reader methods succeed, an error is raised.
+#'
+#' @export
+read_nisurface.fsnative <- function(filepath, ...) {
+  # try to read via read.fs.surface
+  res <- tryCatch({
+    freesurferformats::read.fs.surface(filepath, ...);
+  }, error = function(e) {
+    NULL;
+  });
+
+  # On success, return the surface.
+  if(!is.null(res)){
+    return(res);
+  }
+
+  # Failed, use the next read.ni.surface.* method
+  NextMethod('read_nisurface');
+}
+
+
+#' @title Read a FreeSurfer ASCII surface file.
+#'
+#' @param filepath character string, the full path to the input surface file.
+#'
+#' @param ... parameters passed to \code{\link[freesurferformats]{read.fs.surface.asc}}.
+#'
+#' @return an instance of `fs.surface`, read from the file. See \code{\link[freesurferformats]{read.fs.surface}} for details. If none of the reader methods succeed, an error is raised.
+#'
+#' @export
+read_nisurface.fsascii <- function(filepath, ...) {
+  res <- tryCatch({
+    freesurferformats::read.fs.surface.asc(filepath, ...);
+  }, error = function(e) {
+    NULL;
+  });
+
+  # On success, return the surface.
+  if(!is.null(res)){
+    return(res);
+  }
+
+  # Failed, use the next read.ni.surface.* method
+  NextMethod('read_nisurface');
+}
+
+
+#' @title Read a gifti file as a surface.
+#'
+#' @param filepath character string, the full path to the input surface file.
+#'
+#' @param ... ignored
+#'
+#' @return an instance of `fs.surface`, read from the file. See \code{\link[freesurferformats]{read.fs.surface}} for details. If none of the reader methods succeed, an error is raised.
+#'
+#' @export
+read_nisurface.gifti <- function(filepath, ...) {
+  if (requireNamespace("gifti", quietly = TRUE)) {
+    # Try to read via gifti package
+    res <- tryCatch({
+      gifti::read_gifti(filepath);
+    }, error = function(e) {
+      NULL;
+    });
+
+  } else {   # Won't work without the 'gifti' package
+    res = NULL;
+  }
+
+  # On success, return the result as a surface.
+  if(!is.null(res)){
+    ret_list = list("vertices"=res$data$pointset, "faces"=res$data$triangle + 1);
+    class(ret_list) = c("fs.surface", class(ret_list));
+    return(ret_list);
+  }
+
+  # Failed, use the next read.ni.surface.* method
+  NextMethod('read_nisurface');
+}
+
+
+#' @title Read FreeSurfer ASCII format surface.
+#'
+#' @param filepath string. Full path to the input surface file in ASCII surface format.
+#'
+#' @param metadata named list of arbitrary metadata to store in the instance.
+#'
+#' @return named list. The list has the following named entries: "vertices": nx3 double matrix, where n is the number of vertices. Each row contains the x,y,z coordinates of a single vertex. "faces": nx3 integer matrix. Each row contains the vertex indices of the 3 vertices defining the face. WARNING: The indices are returned starting with index 1 (as used in GNU R). Keep in mind that you need to adjust the index (by substracting 1) to compare with data from other software. "vertex_indices_fs": list of n integers, where n is the number of vertices. The FreeSurfer vertex indices for the vertices.
+#'
+#' @family mesh functions
+#'
+#' @export
+read.fs.surface.asc <- function(filepath, metadata=list()) {
+  num_verts_and_faces_df = read.table(filepath, skip=1L, nrows=1L, col.names = c('num_verts', 'num_faces'), colClasses = c("integer", "integer"));
+  num_verts = num_verts_and_faces_df$num_verts[1];
+  num_faces = num_verts_and_faces_df$num_faces[1];
+
+  vertices_df = read.table(filepath, skip=2L, col.names = c('coord1', 'coord2', 'coord3', 'value'), colClasses = c("numeric", "numeric", "numeric", "numeric"), nrows=num_verts);
+
+  faces_df = read.table(filepath, skip=2L + num_verts, col.names = c('vertex1', 'vertex2', 'vertex3', 'value'), colClasses = c("integer", "integer", "integer", "numeric"), nrows=num_faces);
+
+  ret_list = list();
+  ret_list$vertices = data.matrix(vertices_df[1:3]);
+  ret_list$vertex_indices_fs = 0L:(nrow(ret_list$vertices)-1);
+  ret_list$faces = data.matrix(faces_df[1:3]) + 1;  # the +1 is because the surface should use R indices (one-based)
+  class(ret_list) = c("fs.surface", class(ret_list));
+
+  if(nrow(ret_list$vertices) != num_verts) {
+    stop(sprintf("Expected %d vertices in ASCII surface file '%s' from header, but received %d.\n", num_verts, filepath, nrow(ret_list$vertices)));
+  }
+  if(nrow(ret_list$faces) != num_faces) {
+    stop(sprintf("Expected %d faces in ASCII surface file '%s' from header, but received %d.\n", num_faces, filepath, nrow(ret_list$faces)));
+  }
+
+  return(ret_list);
+}
+
+
 #' @title Read file in FreeSurfer surface format
 #'
 #' @description Read a brain surface mesh consisting of vertex and face data from a file in FreeSurfer binary surface format. For a subject (MRI image pre-processed with FreeSurfer) named 'bert', an example file would be 'bert/surf/lh.white'.
 #'
-#' @param filepath string. Full path to the input curv file. Note: gzipped files are supported and gz format is assumed if the filepath ends with ".gz".
+#' @param filepath string. Full path to the input surface file. Note: gzipped files are supported and gz format is assumed if the filepath ends with ".gz".
 #'
 #' @param metadata named list of arbitrary metadata to store in the instance.
 #'
