c7ffd3744a378eefd454979d2c5108e115180c4b
Tim Schaefer <ts+code@rcmd.org>
2020-01-17 11:59:41 +0100

    improve unit test for read_nisurface

--- tests/testthat/test-read-nisurface.R
+++ tests/testthat/test-read-nisurface.R
@@ -1,6 +1,6 @@
 
 
-test_that("A surface file in FreeSurfer binary format can be read using read_nisurface", {
+test_that("A surface file in FreeSurfer binary format can be read using read_nisurfacefile", {
 
   fsbin_surface_file = system.file("extdata", "lh.tinysurface", package = "freesurferformats", mustWork = TRUE);
   surf = read_nisurfacefile(fsbin_surface_file);
@@ -24,7 +24,7 @@ test_that("A surface file in FreeSurfer binary format can be read using read_nis
 })
 
 
-test_that("A surface file in FreeSurfer ASCII format can be read using read_nisurface", {
+test_that("A surface file in FreeSurfer ASCII format can be read using read_nisurfacefile", {
 
   fsasc_surface_file = system.file("extdata", "lh.tinysurface.asc", package = "freesurferformats", mustWork = TRUE);
   surf = read_nisurfacefile(fsasc_surface_file);
@@ -52,7 +52,8 @@ test_that("A surface files is discovered and read from its basename by read_nisu
   path_including_basename = file.path(dirname(fsasc_surface_file), 'lh.testsurface');
 
   # Note: a file named like 'path_including_basename' does not exits. The read_nisurface function will check for
-  # a number of surface file extensions (indlucing '.asc', the one that exists) and still discover and read the file.
+  # a number of surface file extensions (indlucing '.asc', the one that exists) and still discover and read the file:
+  expect_false(file.exists(path_including_basename));
   surf = read_nisurface(path_including_basename);
 
   known_vertex_count = 5L;
