bb98c2ade0992380112bc941b779176e3e6adaef
Tim Schaefer <ts+code@rcmd.org>
2020-02-25 18:34:49 +0100

    found rotation for example volume nifti data to fit mgh data

--- tests/testthat/test-mgh2nii.R
+++ tests/testthat/test-mgh2nii.R
@@ -10,11 +10,28 @@ test_that("An oro.nifti instance can be converted into an fs.volume instance", {
 
   mgh_from_nii = fs.volume.from.oro.nifti(nii);
 
+
   expect_equal(dim(mgh_from_nii$data), dim(mgh$data));
   expect_equal(mghheader.vox2ras(mgh_from_nii), mghheader.vox2ras(mgh));
 
-  expect_equal(mgh_from_nii$data, mgh$data);
-
   expect_equal(mghheader.crs.orientation(mgh), "LIA");
   expect_equal(mghheader.crs.orientation(mgh_from_nii), "LIA");
+
+  # Check required rotation empirically. We should be able to get this from the NIFTI spec, but
+  # it is not very clear to me. See Q15 here: https://nifti.nimh.nih.gov/nifti-1/documentation/faq#Q3
+  # And see the state of NIFTI support in FS, btw: https://nifti.nimh.nih.gov/nifti-1/support/FreeSurferandNIfTI1
+  mghdata = drop(mgh$data);
+  niidata = drop(mgh_from_nii$data);
+
+  # The following transformation results in the correct orientation for our test volume.
+  # It was found empirically, and this may or may not apply to other volumes. This is WIP.
+  rotated_nii_data = fsbrain::rotate3D(niidata, 1);
+
+  expect_equal(rotated_nii_data, mghdata);
+
+
+  #all.equal(fsbrain::rotate3D(niidata, 1), mghdata);     # TRUE
+  #all.equal(fsbrain::flip3D(niidata, 1), mghdata);       # FALSE
+
+
 })
