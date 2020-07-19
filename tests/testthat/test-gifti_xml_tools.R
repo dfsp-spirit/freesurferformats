88ceb245d3c9f3da5ca21a0fd717f69002b88e3d
Tim Schäfer <ts+code@rcmd.org>
2020-07-19 12:07:55 +0200

    add more tests

--- tests/testthat/test-gifti_xml_tools.R
+++ tests/testthat/test-gifti_xml_tools.R
@@ -14,5 +14,18 @@ test_that("We can generate a GIFTI transform matrix node", {
   tf_matrix = diag(4);
   tf_node = xml_node_gifti_coordtransform(tf_matrix);
 
-  expect_equal(1L, 1L); # add test to prevent skip
+  expect_error(xml_node_gifti_coordtransform(tf_matrix, data_space = 3)); # space must be string
+  expect_error(xml_node_gifti_coordtransform(tf_matrix, transformed_space = 3)); # same
+  expect_error(xml_node_gifti_coordtransform(123)); # not a matrix
+  expect_error(xml_node_gifti_coordtransform(diag(3))); # wrong matrix dim
 })
+
+
+test_that("CDATA encapsulation of strings works", {
+  cdata_start_tag = "<![CDATA[";
+  cdata_end_tag = "]]>";
+
+  expect_error(cdata(cdata_start_tag));
+  expect_error(cdata(cdata_end_tag));
+})
+
