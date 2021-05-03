67da1b985e7364d600ec871b0de9c96d12b0987d
Tim Schäfer <ts+code@rcmd.org>
2021-05-03 07:07:12 +0200

    work on dist to mesh funtion

--- R/read_fs_surface.R
+++ R/read_fs_surface.R
@@ -1422,12 +1422,58 @@ int.to.col.brainvoyager <- function(int_val) {
 
 #' @title Find vertex index closest to given query coordinate.
 #'
-#' @param surface an fs.surface instance
+#' @param surface an fs.surface instance or a nx3 numerical matrix representing mesh points.
 #'
-#' @param coords nx3 matrix of query coords, or a single 3-element vector for a single query point.
+#' @param coords nx3 matrix of query coords. If a vector, will be transformed \code{byrow} to such a matrix.
 #'
 #' @export
-closest.vert.to.coords <- function(surface, coords) {
+#' @importFrom stats dist
+closest.vert.to.point <- function(surface, point_coords) {
 
+  # Check params and extract vertex coords if needed.
+  if(is.fs.surface(surface)) {
+    vertices = surface$vertices;
+  } else {
+    if(is.matrix(surface)) {
+      if(ncol(surface) == 3L) {
+        vertices = surface;
+      } else {
+        stop("Matrix in parameter 'surface' must have 3 columns.");
+      }
+    } else {
+      stop("Parameter 'surface' must be a numerical matrix or an fs.surface instance.");
+    }
+  }
+
+  if(is.vector(point_coords)) {
+    point_coords = matrix(point_coords, ncol = 3L, byrow = TRUE);
+  }
+  if(ncol(point_coords) != 3L) {
+    stop("Parameter coords must have 3 columns.");
+  }
+
+  num_query_points = nrow(point_coords);
+  if(num_query_points < 1L) {
+    stop("Parameter 'point_coords' must not be empty.");
+  }
+
+  results = rep(NA, num_query_points);
+
+  for(point_idx in seq(num_query_points)) {
+    point = point_coords[point_idx, ];
+    dists = apply(vertices, 1, euclidian.dist, point);
+    results[point_idx] = which.min(dists);
+  }
+
+  return(results);
 }
 
+
+#' @title Compute Euclidian distance.
+#'
+#' @param x1 numerical vector, coords of first point
+#'
+#' @param x2 numerical vector, coords of second point
+#'
+#' @keywords internal
+euclidian.dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
