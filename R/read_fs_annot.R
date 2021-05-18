1e2323c459baafd28f5d978042f8825c629c3d4f
Tim Schaefer <ts+code@rcmd.org>
2021-05-18 12:45:12 +0200

    add comment on howto test annot writing

--- R/read_fs_annot.R
+++ R/read_fs_annot.R
@@ -591,9 +591,15 @@ annot.max.region.idx <- function(annot) {
   }
 }
 
+
+
+
 # lh_annot = read.fs.annot("~/data/tim_only/tim/label/lh.aparc.annot");
 # rh_annot = read.fs.annot("~/data/tim_only/tim/label/rh.aparc.annot");
-
+# rh_annot_mod = annot.unique(...
+# write.fs.annot("~/test.annot", annot=rh_annot_mod);
+# To test whether the annot is valid and freesurfer can read it, try converting with mris_convert:
+# $ mris_convert --annot ~/test.annot data/tim_only/tim/surf/rh.white rh.aparc.gii
 
 #' @title Make the region names and indices unique across hemispheres for a parcellation.
 #'
