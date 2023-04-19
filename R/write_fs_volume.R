de73986a639152b547049edf41210b722a4c9781
Tim Schäfer <ts+code@rcmd.org>
2023-04-19 17:51:26 +0200

    CHG: add m44 to quaternion func

--- R/write_fs_volume.R
+++ R/write_fs_volume.R
@@ -27,6 +27,65 @@ write.fs.volume <- function(filepath, fs_vol, format="mgh") {
 }
 
 
+#' @title Compute quaternion representation of a rotation from a 4x4 rotation matrix.
+#'
+#' @param m the input 4x4 matrix encoding the rotation
+#'
+#' @return the quaternion representation
+#'
+#' @keywords internal
+m44_to_quaternion <- function(m) {
+  m00 <- m[1, 1]
+  m01 <- m[1, 2]
+  m02 <- m[1, 3]
+  m10 <- m[2, 1]
+  m11 <- m[2, 2]
+  m12 <- m[2, 3]
+  m20 <- m[3, 1]
+  m21 <- m[3, 2]
+  m22 <- m[3, 3]
+
+  tr <- m00 + m11 + m22 + 1.0
+
+  # https://github.com/NIFTI-Imaging/nifti_clib/blob/d05a71ecdc0b811a509b77c3e82fd0674e901807/niftilib/nifti1_io.c
+  if( tr > 0.5 ) {
+    S <- sqrt( tr ) * 2.0
+    qw <- 0.25 * S
+    qx <- (m21 - m12) / S
+    qy <- (m02 - m20) / S
+    qz <- (m10 - m01) / S
+  } else {
+    Sx <- sqrt(1.0 + m00 - m11 - m22) * 2.0 # S = 4 * qx
+    Sy <- sqrt(1.0 + m11 - m00 - m22) * 2.0 # S = 4 * qy
+    Sz <- sqrt(1.0 + m22 - m00 - m11) * 2.0 # S = 4 * qz
+
+    if( Sx > 2.0 ) {
+      qw <- (m21 - m12) / Sx
+      qx <- 0.25 * Sx
+      qy <- (m01 + m10) / Sx
+      qz <- (m02 + m20) / Sx
+    } else if ( Sy > 2.0 ) {
+      qw <- (m02 - m20) / Sy
+      qx <- (m01 + m10) / Sy
+      qy <- 0.25 * Sy
+      qz <- (m12 + m21) / Sy
+    } else {
+      qw <- (m10 - m01) / Sz
+      qx <- (m02 + m20) / Sz
+      qy <- (m12 + m21) / Sz
+      qz <- 0.25 * Sz
+    }
+
+    if( tr < 0.0 ) {
+      qx <- -qx
+      qy <- -qy
+      qz <- -qz
+    }
+
+  }
+  return(c(qw, qx, qy, qz))
+}
+
 #' @title Create a NIFTI 1 header from the header information contained in an fs.volume instance.
 #'
 #' @param an fs.volume instance, or a string. If a string, it is interpreted as a filepath to a volume file (NIFTI, MGH or MGZ) that should be loaded.
